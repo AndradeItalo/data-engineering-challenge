514b8fa3dbfceb376e9804941e3374589ebbe767
Italo Andrade <italo.4012@gmail.com>
2026-07-18 09:58:58 -0300

    wip: ajuste path gitignore

--- urban-mobility-analytics/docker/postgres-init/00_create_airflow_db.sql
+++ urban-mobility-analytics/docker/postgres-init/00_create_airflow_db.sql
@@ -1,3 +1,3 @@
--- Roda só na 1a inicialização do volume (docker-entrypoint-initdb.d).
+-- Roda só na primeira inicialização do volume (docker-entrypoint-initdb.d)
 -- Metadados do Airflow ficam separados do banco analítico (urban_mobility).
 CREATE DATABASE airflow;
